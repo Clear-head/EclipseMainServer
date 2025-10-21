c91ffd7464cf924344cabe96d502f8e89f90e3e9
Clear-head <huni032285@gmail.com>
2025-10-21 17:21:34 +0900

    [fix] db 연결 되도록 수정

--- src/infra/database/repository/maria_engine.py
+++ src/infra/database/repository/maria_engine.py
@@ -1,5 +1,10 @@
+import asyncio
+import urllib
 from json import load
 from pathlib import Path
+from urllib.parse import quote_plus
+
+from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 _ENGINE = None
@@ -13,20 +18,38 @@ async def get_engine() -> AsyncEngine:
 
     try:
 
-        config_path = Path(__file__).parent.parent / 'resources' / 'config' / 'database_config.json'
+        config_path = Path(__file__).parent.parent.parent.parent.joinpath('resources').joinpath('config').joinpath('database_config.json')
 
         with open(config_path) as f:
-            config = load(f)
+            config = load(f)["maria"]
 
-        _ENGINE = create_async_engine(
-            f"mysql+aiomysql://{config['user']}:{config['password']}"
-            f"@{config['host']}:{config['port']}/{config['database']}",
-            pool_size=config.get('pool_size', 5),
-            pool_pre_ping=config.get('pool_pre_ping', True),
-            max_overflow=config.get('max_overflow', 10)
-        )
+            _ENGINE = create_async_engine(
+                f'mysql+asyncmy://{config["user"]}:{config["password"]}@{config["host"]}:{config["port"]}/{config["database"]}'
+            )
+
+            # password = urllib.parse.quote_plus(config['password'])
+
+            # _ENGINE = create_async_engine(
+            #     f"mysql+aiomysql://{config['user']}:{password}"
+            #     f"@{config['host']}:{config['port']}/{config['database']}",
+            #     pool_size=config.get('pool_size', 5),
+            #     pool_pre_ping=config.get('pool_pre_ping', True),
+            #     max_overflow=config.get('max_overflow', 10)
+            # )
+
+            # url_object = URL.create(
+            #     "mysql+aiomysql",
+            #     username=config["user"],
+            #     password=config["password"],
+            #     host=config["host"],
+            #     port=config["port"],
+            #     database=config["database"],
+            # )
+            #
+            # _ENGINE = create_async_engine(url_object)
 
         return _ENGINE
 
     except Exception as e:
-        raise e
+        print(e)
+        raise Exception(e)
