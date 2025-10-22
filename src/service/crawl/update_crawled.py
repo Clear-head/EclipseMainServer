1465b40f4df4f2d95270250510ada00f458f1dd5
Clear-head <huni032285@gmail.com>
2025-10-22 12:46:29 +0900

    [feat] 크롤링 update 서비스

--- src/service/crawl/update_crawled.py
+++ src/service/crawl/update_crawled.py
@@ -1,52 +1,63 @@
-import asyncio
-
 from src.domain.dto.insert_category_dto import InsertCategoryDto
+from src.domain.dto.insert_category_tags_dto import InsertCategoryTagsDTO
 from src.domain.entities.category_entity import CategoryEntity
+from src.domain.entities.category_tags_entity import CategoryTagsEntity
 from src.infra.database.repository.category_repository import CategoryRepository
-from src.infra.database.repository.tags_repository import TagsRepository
+from src.infra.database.repository.category_tags_repository import CategoryTagsRepository
 from src.logger.logger_handler import get_logger
 
 logger = get_logger(__name__)
-async def update_category(dto: InsertCategoryDto) -> int:
-    logger.info(f"Updating category: {dto.name}")
+async def update_category(dto: InsertCategoryDto) -> str:
+    try:
+        logger.info(f"Updating category: {dto.name}")
 
-    repository = CategoryRepository()
-    result = await repository.select_by(name=dto.name, type=dto.type)
+        repository = CategoryRepository()
+        result = await repository.select_by(name=dto.name, type=dto.type, detail_address=dto.detail_address)
 
-    #   항목 중복 업데이트 불가
-    if len(result) > 1:
-        logger.error(f"Found {len(result)} results for category {dto.name}")
-        return -1
-    elif len(result) == 0:
-        logger.error(f"No results for category {dto.name} plz insert category")
-        return 0
-    else:
-        id = result[0].id
-        entity = CategoryEntity.from_dto(result[0], id)
-        await repository.update(id, entity)
+        #   항목 중복 or 없어서 업데이트 불가
+        if len(result) != 1:
+            raise Exception(f"Found {len(result)} results for category {dto.name}")
 
-        logger.info(f"successful Updated category: {dto.name}")
-        return 1
+        else:
+            id = result[0].id
+            entity = CategoryEntity.from_dto(result[0], id)
+            flag = await repository.update(id, entity)
 
+            if flag:
+                logger.info(f"successful Updated category: {dto}")
+                return id
+            else:
+                logger.info(f"failed Updated category: {dto}")
+                raise Exception(f"failed Updated category: {dto.id}")
 
-async def update_tags(name: str):
-    logger.info(f"Updating tags")
+    except Exception as e:
+        logger.error(e)
+        raise Exception(f"update category error: {dto.name, e}")
 
-    repository = TagsRepository()
+async def update_category_tags(dto: InsertCategoryTagsDTO) -> int:
+    try:
+        logger.info(f"Updating category tags")
+        repository = CategoryTagsRepository()
+        result = await repository.select_by(tag_id=dto.tag_id, category_id=dto.category_id)
 
-    a = await repository.select_by(name=name)
+        #   중복이거나 항목 없으면 error
+        if len(result) != 1:
+            raise Exception(f"Found {len(result)} results ")
 
-    if len(a) > 1:
-        logger.error(f"Found {len(a)} results for tag {name}")
-        return -1
-    elif len(a) == 0:
-        logger.error(f"No results for tag {name} plz insert tag")
-        return 0
-    else:
-        id = a[0].id
-        item = {"name": name, "id": id}
-        await repository.update(id, item)
-        return 1
+        else:
+            id = result[0].id
+            entity = CategoryTagsEntity.from_dto(dto, id=id)
+            print(entity)
+            flag = await repository.update(id, entity)
 
-async def update_category_tags():
-    logger.info(f"Updating category tags")
+            if flag:
+                logger.info(f"successful Updated category tags")
+                return id
+
+            else:
+                logger.info(f"failed Updated category tags:")
+                raise Exception(f"failed Updated category tags")
+
+    except Exception as e:
+        logger.error(e)
+        raise Exception(f"update category tags error: {e}")
