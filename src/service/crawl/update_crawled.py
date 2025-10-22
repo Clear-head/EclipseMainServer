2933c63753a420cea9fd0dbd544ab2594dcd85c0
telkem <suu692013@gmail.com>
2025-10-22 16:07:22 +0900

    [fix] DB update 시도 성공, insert 할때 await 추가

--- src/service/crawl/update_crawled.py
+++ src/service/crawl/update_crawled.py
@@ -20,7 +20,9 @@ async def update_category(dto: InsertCategoryDto) -> str:
 
         else:
             id = result[0].id
-            entity = CategoryEntity.from_dto(result[0], id)
+            print("TEST1")
+            entity = CategoryEntity.from_dto(dto, id=id)
+            print("TEST2")
             flag = await repository.update(id, entity)
 
             if flag:
