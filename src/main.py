735b8c2f65982da096c6f0789c4b567ea057bc12
Clear-head <huni032285@gmail.com>
2025-10-24 12:00:52 +0900

    [chore] 폴더 구조 변경에 따른 import 문 변경

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+
+from src.router.users import user_controller, service_controller
 
 
 @asynccontextmanager
@@ -10,7 +14,18 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(user_controller.router)
+app.include_router(service_controller.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
