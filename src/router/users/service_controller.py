72e7a3428a3e5b2f45d258d2e14f305f1cd223bb
Clear-head <huni032285@gmail.com>
2025-10-28 10:16:08 +0900

    [refactor] 리프레시 토큰 api 유저 컨트롤러로 이동

--- src/router/users/service_controller.py
+++ src/router/users/service_controller.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from src.domain.dto.service.main_screen_dto import RequestMainScreenDTO
 from src.domain.dto.service.request_jwt_dto import RequestAccessTokenDto
 from src.logger.custom_logger import get_logger
-from src.service.application.main_service import MainService
+from src.service.application.main_screen_service import MainScreenService
 from src.service.auth.jwt import validate_jwt_token
 from src.utils.exception_handler.auth_error_class import MissingTokenException, ExpiredAccessTokenException, \
     ExpiredRefreshTokenException
@@ -13,7 +13,7 @@ router = APIRouter(prefix="/api/service")
 logger = get_logger(__name__)
 
 
-
+#   메인 화면: 로그인 후 바로 보여지는 화면
 @router.post("/main")
 @router.get("/main")
 async def to_main_screen(dto: RequestMainScreenDTO):
@@ -25,24 +25,9 @@ async def to_main_screen(dto: RequestMainScreenDTO):
 
     validate_result = await validate_jwt_token(jwt)
 
-    if validate_result is 2:
+    if validate_result == 2:
         logger.error("ExpiredToken token")
         raise ExpiredAccessTokenException()
 
-    main_service_class = MainService()
+    main_service_class = MainScreenService()
     return await main_service_class.to_main()
-
-
-@router.get("/refresh")
-async def to_refresh(dto: RequestAccessTokenDto):
-    jwt = dto.header.jwt
-
-    if jwt is None:
-        logger.error("Missing token")
-        raise MissingTokenException()
-
-    validate_result = await validate_jwt_token(jwt)
-    if validate_result is 2:
-        #   todo: 세션에서 삭제
-        #       로그아웃 까지
-        raise ExpiredRefreshTokenException()
--- src/router/users/user_controller.py
+++ src/router/users/user_controller.py
@@ -1,11 +1,14 @@
 import traceback
 
 from fastapi import APIRouter
+
+from src.domain.dto.service.request_jwt_dto import RequestAccessTokenDto
 from src.domain.dto.service.user_login_dto import GetUserLoginDto
 from src.domain.dto.service.user_register_dto import RequestRegisterBody
 from src.logger.custom_logger import get_logger
 from src.service.application.user_service import UserService
-
+from src.service.auth.jwt import validate_jwt_token
+from src.utils.exception_handler.auth_error_class import MissingTokenException, ExpiredRefreshTokenException
 
 router = APIRouter(prefix="/api/users", tags=["users"])
 logger = get_logger(__name__)
@@ -59,3 +62,19 @@ async def find_user_id():
 @router.post('/password')
 async def find_user_pw():
     pass
+
+
+#   refresh jwt
+@router.get("/refresh")
+async def to_refresh(dto: RequestAccessTokenDto):
+    jwt = dto.header.jwt
+
+    if jwt is None:
+        logger.error("Missing token")
+        raise MissingTokenException()
+
+    validate_result = await validate_jwt_token(jwt)
+    if validate_result == 2:
+        #   todo: 세션에서 삭제
+        #       로그아웃 까지
+        raise ExpiredRefreshTokenException()
