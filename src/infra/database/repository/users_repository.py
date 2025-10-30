d3369cfff4b0e1e5dae08303ac01968350dc2f12
Clear-head <huni032285@gmail.com>
2025-10-30 15:34:06 +0900

    [refactor] repository select 함수 통합 에 따른 함수변경

--- src/infra/database/repository/users_repository.py
+++ src/infra/database/repository/users_repository.py
@@ -1,10 +1,6 @@
-from sqlalchemy import select
-from sqlalchemy.util import await_only
-
-from . import base_repository
-from .maria_engine import get_engine
-from ..tables.table_users import users_table
 from src.domain.entities.user_entity import UserEntity
+from . import base_repository
+from ..tables.table_users import users_table
 
 
 class UserRepository(base_repository.BaseRepository):
@@ -16,37 +12,11 @@ class UserRepository(base_repository.BaseRepository):
     async def insert(self, item):
         return await super().insert(item)
 
-    async def select(self, item, limit=None):
-        return await super().select(item, limit=limit)
+    async def select(self, **filters):
+        return await super().select(**filters)
 
     async def update(self, item_id, item):
         return await super().update(item_id, item)
 
     async def delete(self, item):
         return await super().delete(item)
-
-    async def select_by(self, **filters):
-        return await super().select_by(**filters)
-
-    async def select_with_join(self, user_id, join_table, dto, join_conditions: dict, **filters) -> list:
-        return await super().select_with_join(user_id, join_table, dto, join_conditions, **filters)
-
-    async def get_user_info(self, user_id: str):
-        """
-
-                                하루가 쓸 정보
-
-        """
-        try:
-            engine = await get_engine()
-            async with engine.begin() as conn:
-                stmt = select(self.table.c).where(self.table.c.id == user_id)
-                result = await conn.execute(stmt)
-                user_entity = self.entity(**result.one())
-        except Exception as e:
-            self.logger.error(e)
-            raise Exception("[UserRepository] select error") from e
-
-        return user_entity
-
-
