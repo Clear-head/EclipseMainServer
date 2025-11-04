4fe9c54f6a1e957008820ddd00105c1c68a3ca6f
Clear-head <huni032285@gmail.com>
2025-11-04 16:46:38 +0900

    [fix] 테이블 내부 컬럼 이름이 예약어여서 선언 안되던 문제

--- src/infra/database/tables/table_user_history.py
+++ src/infra/database/tables/table_user_history.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Column, String, DateTime, Table, ForeignKey, Text, Integer
+from sqlalchemy import MetaData, Column, String, DateTime, Table, ForeignKey, Integer
 
 meta = MetaData()
 
@@ -6,7 +6,8 @@ user_history_table = Table(
     'user_history',
     meta,
     Column('id', String, primary_key=True),
-    Column('order', String(1), primary_key=True),
+    Column('merge_id', Integer, ForeignKey('merge_history.id')),
+    Column('seq', Integer),
     Column('visited_at', DateTime, nullable=False),
     Column('user_id', String(255), ForeignKey('users.id'), nullable=False),
     Column('category_id', String(255), ForeignKey('category.id')),
@@ -14,14 +15,3 @@ user_history_table = Table(
     Column("duration", Integer),
     Column("transportation", String(1))
 )
-
-# Table "user_history" {
-#   "id" VARCHAR(255) [pk]
-#   "order" char(1)
-#   "visited_at" datetime
-#   "user_id" VARCHAR(255)
-#   "category_id" varchar(255)
-#   "category_name" varchar(255)
-#   "duration" int
-#   "transportation" char(1)
-# }
