c7698f8a66f1ba9e073400212dbc0d9c8b2d633d
telkem <suu692013@gmail.com>
2025-11-17 14:04:37 +0900

    [fix] 파일명 바꾸고 유저 정보 안바뀌던거 수정

--- src/router/users/user_controller.py
+++ src/router/users/user_controller.py
@@ -4,17 +4,18 @@ from starlette.responses import JSONResponse
 from src.domain.dto.user.user_account_dto import RequestDeleteAccountDTO
 from src.domain.dto.user.user_profile_dto import RequestUpdateProfileDTO
 from src.logger.custom_logger import get_logger
-from src.router.users.auth_controller import user_service
-from src.router.users.my_info_controller import user_info
+from src.service.user.user_service import UserService
 from src.service.auth.jwt import get_jwt_user_id
 
 router = APIRouter(prefix="/api/users", tags=["users"])
 logger = get_logger(__name__)
 
+user_service = UserService()
+
 
 @router.put("/me/{field}")
 async def change_info(field: str, dto: RequestUpdateProfileDTO, user_id:str = Depends(get_jwt_user_id)):
-    return await user_info.change_info(dto, field, user_id)
+    return await user_service.change_info(dto, field, user_id)
 
 
 #   회원탈퇴
